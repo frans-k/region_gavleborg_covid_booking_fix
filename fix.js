454f1424406c3d4b7af8024c6ae5020d86a7e9ca
Frans Krojegård <frans@krojegard.com>
2021-06-27 09:52:05 +0200

    Move into functions and use promises to wait for date retrieval before sorting.

--- fix.js
+++ fix.js
@@ -49,56 +49,62 @@ async function getCareUnits() {
     return await response.json()
 }
 
-getCareUnits().then(cus => {
-    cus.forEach(cu => {
-        // console.log(cu.careUnit)
-        getFreeBookings(cu.careUnit.id).then(careUnitWithDates => {
-            const listItemText = [...document.querySelectorAll("strong")]
-                .filter(a => a.textContent.includes(careUnitWithDates.careUnit.name))[0]
-            console.log(careUnitWithDates)
-            console.log(careUnitWithDates.dates)
-            if (Object.keys(careUnitWithDates.dates).length == 0) {
-                const span = document.createElement('span')
-                span.style = 'color: red'
-                span.textContent = 'TOM - '
-                listItemText.prepend(span)
-                // listItem.closest('[class^=AddressListPage_itemContainer]')
-            }
-        })
+function getEmptiness () {
+    return getCareUnits().then(cus => {
+        const promises = cus.map(cu => {
+            // console.log(cu.careUnit)
+            return getFreeBookings(cu.careUnit.id).then(careUnitWithDates => {
+                const listItemText = [...document.querySelectorAll("strong")]
+                    .filter(a => a.textContent.includes(careUnitWithDates.careUnit.name))[0]
+                // console.log(careUnitWithDates)
+                // console.log(careUnitWithDates.dates)
+                if (Object.keys(careUnitWithDates.dates).length == 0) {
+                    const span = document.createElement('span')
+                    span.style = 'color: red'
+                    span.textContent = 'TOM - '
+                    listItemText.prepend(span)
+                    // listItem.closest('[class^=AddressListPage_itemContainer]')
+                }
+            })
+        });
+        return Promise.all(promises)
+    })
+}
+
+
+function sortList () {
+    let lists = document.querySelectorAll('[class^=AddressListPage_rows]')
+    let longestList = undefined
+    let longest = 0
+    lists.forEach(list => {
+        if (list.childNodes.length > longest) {
+            longestList = list
+        }
     });
-})
 
-
-
-let lists = document.querySelectorAll('[class^=AddressListPage_rows]')
-let longestList = undefined
-let longest = 0
-lists.forEach(list => {
-    if (list.childNodes.length > longest) {
-        longestList = list
+    let items = longestList.childNodes;
+    let itemsArr = [];
+    for (let i in items) {
+        if (items[i].nodeType == 1) { // get rid of the whitespace text nodes
+            itemsArr.push(items[i]);
+        }
     }
-});
 
-let items = longestList.childNodes;
-let itemsArr = [];
-for (let i in items) {
-    if (items[i].nodeType == 1) { // get rid of the whitespace text nodes
-        itemsArr.push(items[i]);
+    itemsArr.sort(function (a, b) {
+        if (a.innerText.includes('Välj')) {
+            return -1
+        }
+        if (b.innerText.includes('Välj')) {
+            return 1
+        }
+        return a.innerText == b.innerText
+            ? 0
+            : (a.innerText > b.innerText ? 1 : -1);
+    });
+
+    for (i = 0; i < itemsArr.length; ++i) {
+        longestList.appendChild(itemsArr[i]);
     }
 }
 
-itemsArr.sort(function (a, b) {
-    if (a.innerText.includes('Välj')) {
-        return -1
-    }
-    if (b.innerText.includes('Välj')) {
-        return 1
-    }
-    return a.innerText == b.innerText
-        ? 0
-        : (a.innerText > b.innerText ? 1 : -1);
-});
-
-for (i = 0; i < itemsArr.length; ++i) {
-    longestList.appendChild(itemsArr[i]);
-}
+getEmptiness().then(sortList)
